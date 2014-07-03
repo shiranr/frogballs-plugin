3dad1a23880e7d7c06e817ec92d372b9a931ad18
shiranr <shiranr@jfrog.com>
2014-07-03 10:51:46 +0300

    * small code changes.

--- src/main/groovy/hudson/plugins/frogballs/PluginMain.groovy
+++ src/main/groovy/hudson/plugins/frogballs/PluginMain.groovy
@@ -1,14 +1,14 @@
 package hudson.plugins.frogballs
+
 import hudson.Plugin
 import hudson.util.PluginServletFilter
-import org.kohsuke.stapler.StaplerRequest
-import org.kohsuke.stapler.StaplerResponse
 
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import java.util.logging.Level
 import java.util.logging.Logger
+
 /**
  * Created by Shiran on 7/1/2014.
  */
@@ -25,19 +25,10 @@ class PluginMain extends Plugin {
         logger.log(Level.INFO, "Frog Balls!")
     }
 
-    @Override
-    public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-        rsp.setHeader("Cache-Control", "public, s-maxage=86400")
-        String path = req.restOfPath
-        logger.log(Level.FINE, "Serving cached resource {0}", path)
-        rsp.serveLocalizedFile(req, new URL(wrapper.baseResourceURL, '.' + path), 86400000)
-    }
-
     def filterClass = [
-            doFilter: {ServletRequest request, ServletResponse response, FilterChain chain ->
+            doFilter: { ServletRequest request, ServletResponse response, FilterChain chain ->
                 if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
-                    final String uri = ((HttpServletRequest) request).requestURI
-                    logger.log(Level.INFO, "I'm in wih path ${uri}")
+                    final String uri = (request as HttpServletRequest).requestURI
                     if (uri.contains('health')) {
                         String newImageUrl = mapImage(uri)
                         RequestDispatcher dispatcher = request.getRequestDispatcher newImageUrl
@@ -51,9 +42,8 @@ class PluginMain extends Plugin {
             destroy : {}
     ]
 
-    private String mapImage(String uri) {
-        int index =  uri.findLastIndexOf { it =='/'}
-        logger.log(Level.INFO, uri[index .. uri.size()-1])
-        "/plugin/frogballs/${uri[index .. uri.size()-1]}"
+    String mapImage(String uri) {
+        int index = uri.findLastIndexOf { it == '/' }
+        "/plugin/frogballs/${uri[index..uri.size() - 1]}"
     }
 }
